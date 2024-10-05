0eebd22d8437d91613b00541dbf6ee21df92c1d5
Kazakov-app <kazakov.app@yandex.ru>
2024-10-05 22:25:01 +0300

    05.10.2024 add new classes

---
+++ src/main/java/org/example/application/chapter/second/InputIntVariablesApplication.java
@@ -0,0 +1,24 @@
+package org.example.application.chapter.second;
+
+import javax.swing.*;
+
+public class InputIntVariablesApplication {
+    public static void main(String[] args){
+        //Целочислительная переменные:
+        int age,year,birth;
+        //Переменная для записи значения в поле ввода:
+        String result;
+        //Считывание значения из поля ввода:
+        result=JOptionPane.showInputDialog("Какой сейчас год?");
+        //Преобразование текста в целое число:
+        year=Integer.parseInt(result);
+        //Считывание значения из поля ввода:
+        result=JOptionPane.showInputDialog("Сколько Вам лет?");
+        //Преобразование текста в целое число:
+        age=Integer.parseInt(result);
+        //Вычисление года рождения:
+        birth=year-age;
+        //Отображение окна с сообщением:
+        JOptionPane.showMessageDialog(null,"Вы родились в "+birth+" году!");
+    }
+}
---
+++ src/main/java/org/example/application/chapter/second/UsingVarsConsoleDemo.java
@@ -0,0 +1,24 @@
+package org.example.application.chapter.second;
+
+public class UsingVarsConsoleDemo {
+    public static void main(String[] args){
+        //Целочислительная переменная:
+        int number=123;
+        //Действительная переменная:
+        double x=32.1;
+        //Символьная переменная:
+        char symbol='A';
+        //Логическая переменная:
+        boolean state=false;
+        //Отображение текста:
+        System.out.println("Используемые переменные: ");
+        //Отображение значения целочислительной переменной:
+        System.out.println("Целое число: "+number);
+        //Отображение значения числовой переменной:
+        System.out.println("Действительное число: "+x);
+        //Отображение значения символьной переменной:
+        System.out.println("Символ: "+symbol);
+        //Отображение значения логической переменной:
+        System.out.println("Логическое значение: "+state);
+    }
+}

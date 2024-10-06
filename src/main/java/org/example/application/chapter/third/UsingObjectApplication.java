ec9c25dee50436e05eeeee4399abd5f0b3827e40
Kazakov-app <kazakov.app@yandex.ru>
2024-10-06 20:20:06 +0300

    06.10.2024 add new classes

---
+++ src/main/java/org/example/application/chapter/third/UsingObjectApplication.java
@@ -0,0 +1,27 @@
+package org.example.application.chapter.third;
+
+import javax.swing.*;
+
+//Описание класса:
+class MyClass{
+    //Поля класса:
+    int number;
+    char symbol;
+}
+
+//Описание класса с главным методом программы:
+class UsingObjectDemo {
+    //Главный метод программы:
+    public static void main(String[]args){
+        //Создание объекта
+        MyClass obj=new MyClass();
+        //Присваивание значение полям объекта:
+        obj.number=100;
+        obj.symbol='A';
+        //Текст для отображения в диалоговом окне:
+        String text="Число: "+obj.number+"\n";
+        text+="Символ: "+obj.symbol;
+        //Отображение диалогового окна:
+        JOptionPane.showMessageDialog(null,text);
+    }
+}
